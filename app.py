855e7a6786b17822aef0c762e9633c8a53eaed8f
Kenneth <kenneth.mburu@student.moringaschool.com>
2023-11-07 13:26:24 +0300

    database

--- app.py
+++ app.py
@@ -1,6 +1,13 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, redirect, request, url_for
+from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
+
+db = SQLAlchemy(app)
+
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///database.db'
+
+app.config['SECRET_KEY'] = 'a018ddb39a56d24e179255f0fa7f509ff9b215a905cbf1a15e56a32b465e319a542227eb50d3428de14d51c27c17d775852cf14ab0'
 
 @app.route('/')
 def home():
